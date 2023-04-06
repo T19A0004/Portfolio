d20809b717c6bc2c66ffc5b2336bb30819a6996b
Komiljon <komilriksiyev@gmail.com>
2023-04-06 16:45:40 +0500

    test

---
+++ lang.js
@@ -0,0 +1,38 @@
+const langArr = {
+    "home" :  {
+        "jp": "ホーム",
+        "en": "Home",
+    }, 
+    "about" :  {
+        "jp": "自己紹介",
+        "en": "About",
+    }, 
+    "services" :  {
+        "jp": "サービス",
+        "en": "Services",
+    }, 
+    "portfolio" :  {
+        "jp": "ポートフォリオ",
+        "en": "Portfolio",
+    }, 
+    "contacts" :  {
+        "jp": "連絡先",
+        "en": "Contacts",
+    }, 
+    "about-me" :  {
+        "jp": "自己紹介",
+        "en": "About",
+    }, 
+    "services-my" :  {
+        "jp": "サービス",
+        "en": "Services",
+    }, 
+    "portfolio-my" :  {
+        "jp": "ポートフォリオ",
+        "en": "Portfolio",
+    }, 
+    "contacts-my" :  {
+        "jp": "連絡先",
+        "en": "Contacts",
+    }, 
+}
---
+++ script.js
@@ -0,0 +1,104 @@
+
+const tablinks = document.getElementsByClassName("tab-links");
+const tabcontents = document.getElementsByClassName("tab-contents");
+      
+function opentab(tabname) {
+    for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+       tabcontent.classList.remove("active-tab");
+    }
+     event.currentTarget.classList.add("active-link");
+     document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+// -------------------sideMenu----------------
+
+const sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+
+
+// ----------------contactMessage---------------
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyzdghoqvJZ9x7ovrSw5IZDa8G7hkvpVZS-JhQkS3kywJEbyB4izzAw44nyeFI_dNKvRA/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+
+  // ------------------toTopButton------------------
+
+  let topbutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+// ---------------------switchLanguage-------------------
+
+const select = document.querySelector('select');
+const allLang = ['en', 'jp'];
+
+select.addEventListener('change', changeURLLanguage);
+
+// перенаправить на url с указанием языка
+function changeURLLanguage() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    console.log(hash);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#en';
+        location.reload();
+    }
+    select.value = hash;
+    // document.querySelector('title').innerHTML = langArr['unit'][hash];
+    // document.querySelector('.lng-chip').innerHTML = langArr['chip'][hash];
+    for (let key in langArr) {
+        let elem = document.querySelector('.lng-' + key);
+        if (elem) {
+            elem.innerHTML = langArr[key][hash];
+        }
+    }
+}
+
+changeLanguage();
