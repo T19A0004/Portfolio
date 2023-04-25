bd8b9ab61b3d56cf5e4ac1ce34eaa8bbd430206b
Komiljon <komilriksiyev@gmail.com>
2023-04-25 14:42:30 +0500

    fix

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
-
 const tablinks = document.getElementsByClassName("tab-links");
 const tabcontents = document.getElementsByClassName("tab-contents");
-      
-function opentab(tabname) {
-    for (tablink of tablinks) {
-    tablink.classList.remove("active-link");
-    }
-    for (tabcontent of tabcontents) {
-       tabcontent.classList.remove("active-tab");
-    }
-     event.currentTarget.classList.add("active-link");
-     document.getElementById(tabname).classList.add("active-tab");
-}
 
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
 
 // -------------------sideMenu----------------
 
 const sidemenu = document.getElementById("sidemenu");
 
-function openmenu(){
-    sidemenu.style.right = "0";
+function openmenu() {
+  sidemenu.style.right = "0";
 }
 
-function closemenu(){
-    sidemenu.style.right = "-200px";
+function closemenu() {
+  sidemenu.style.right = "-200px";
 }
-
 
 // ----------------contactMessage---------------
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyzdghoqvJZ9x7ovrSw5IZDa8G7hkvpVZS-JhQkS3kywJEbyB4izzAw44nyeFI_dNKvRA/exec'
-  const form = document.forms['submit-to-google-sheet']
-  const msg = document.getElementById("msg")
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbyzdghoqvJZ9x7ovrSw5IZDa8G7hkvpVZS-JhQkS3kywJEbyB4izzAw44nyeFI_dNKvRA/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-        msg.innerHTML = "Message sent successfully"
-        setTimeout(function(){
-            msg.innerHTML = ""
-        },5000)
-        form.reset()
-      })
-      .catch(error => console.error('Error!', error.message))
-  })
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 1000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
 
-  // ------------------toTopButton------------------
+// ------------------toTopButton------------------
 
-  let topbutton = document.getElementById("topBtn");
+let topbutton = document.getElementById("topBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -67,38 +67,37 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-
 // ---------------------switchLanguage-------------------
 
-const select = document.querySelector('select');
-const allLang = ['en', 'jp'];
+const select = document.querySelector("select");
+const allLang = ["en", "jp"];
 
-select.addEventListener('change', changeURLLanguage);
+select.addEventListener("change", changeURLLanguage);
 
-// перенаправить на url с указанием языка
+// redirect to url
 function changeURLLanguage() {
-    let lang = select.value;
-    location.href = window.location.pathname + '#' + lang;
-    location.reload();
+  let lang = select.value;
+  location.href = window.location.pathname + "#" + lang;
+  location.reload();
 }
 
 function changeLanguage() {
-    let hash = window.location.hash;
-    hash = hash.substr(1);
-    console.log(hash);
-    if (!allLang.includes(hash)) {
-        location.href = window.location.pathname + '#en';
-        location.reload();
+  let hash = window.location.hash;
+  hash = hash.substr(1);
+  console.log(hash);
+  if (!allLang.includes(hash)) {
+    location.href = window.location.pathname + "#en";
+    location.reload();
+  }
+  select.value = hash;
+  // document.querySelector('title').innerHTML = langArr['unit'][hash];
+  // document.querySelector('.lng-chip').innerHTML = langArr['chip'][hash];
+  for (let key in langArr) {
+    let elem = document.querySelector(".lng-" + key);
+    if (elem) {
+      elem.innerHTML = langArr[key][hash];
     }
-    select.value = hash;
-    // document.querySelector('title').innerHTML = langArr['unit'][hash];
-    // document.querySelector('.lng-chip').innerHTML = langArr['chip'][hash];
-    for (let key in langArr) {
-        let elem = document.querySelector('.lng-' + key);
-        if (elem) {
-            elem.innerHTML = langArr[key][hash];
-        }
-    }
+  }
 }
 
 changeLanguage();
