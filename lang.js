5b7448fe868e3127679350d5d855501dae22c822
Komiljon <komilriksiyev@gmail.com>
2023-04-25 18:21:37 +0500

    add some lang

--- lang.js
+++ lang.js
@@ -1,38 +1,58 @@
 const langArr = {
-    "home" :  {
-        "jp": "ホーム",
-        "en": "Home",
-    }, 
-    "about" :  {
-        "jp": "自己紹介",
-        "en": "About",
-    }, 
-    "services" :  {
-        "jp": "サービス",
-        "en": "Services",
-    }, 
-    "portfolio" :  {
-        "jp": "ポートフォリオ",
-        "en": "Portfolio",
-    }, 
-    "contacts" :  {
-        "jp": "連絡先",
-        "en": "Contacts",
-    }, 
-    "about-me" :  {
-        "jp": "自己紹介",
-        "en": "About",
-    }, 
-    "services-my" :  {
-        "jp": "サービス",
-        "en": "Services",
-    }, 
-    "portfolio-my" :  {
-        "jp": "ポートフォリオ",
-        "en": "Portfolio",
-    }, 
-    "contacts-my" :  {
-        "jp": "連絡先",
-        "en": "Contacts",
-    }, 
-}
+  home: {
+    jp: "ホーム",
+    en: "Home",
+  },
+  about: {
+    jp: "自己紹介",
+    en: "About",
+  },
+  "about-me-text": {
+    jp: "ウズベキスタンの首都タシケントに住んでいます。 私はウズベキスタンのJapanDigitalUniversity(JDU)で勉強しています。",
+    en: "I live in Tashkent, the capital of Uzbekistan. I am studying at Japan Digital University (JDU) in Uzbekistan.",
+  },
+  services: {
+    jp: "サービス",
+    en: "Services",
+  },
+  portfolio: {
+    jp: "ポートフォリオ",
+    en: "Portfolio",
+  },
+  contacts: {
+    jp: "連絡先",
+    en: "Contacts",
+  },
+  "about-me": {
+    jp: "自己紹介",
+    en: "About",
+  },
+  "services-my": {
+    jp: "サービス",
+    en: "Services",
+  },
+  "portfolio-my": {
+    jp: "ポートフォリオ",
+    en: "Portfolio",
+  },
+  "contacts-my": {
+    jp: "連絡先",
+    en: "Contacts",
+  },
+  hi: {
+    jp: "こんにちは。私の名前は",
+    en: "Hello. My name is",
+  },
+  name: {
+    jp: "コミルジョン",
+    en: "Komiljon",
+  },
+  and: {
+    jp: "です。私は",
+    en: " and I am ",
+  },
+  country: {
+    jp: "ウズベキスタンに住んでいます",
+    en: "From Uzbekistan",
+  },
+};
